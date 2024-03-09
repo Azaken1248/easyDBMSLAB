a9af9dd33687857c3390211869e0754b7cfdaf9c
Azaken1248 <suvkrm@gmail.com>
2024-03-09 18:47:02 +0530

    Fixed Client Server Communication And Data Transfer

--- servers/testserver.py
+++ servers/testserver.py
@@ -1,9 +1,26 @@
 from flask import Flask, jsonify, request
-from databaseMYSQLver import connection,queryMode
+from databaseMYSQLver import connection
 import json
 
+app = Flask(__name__)
 
-def toJson(header, data):
+# Global variable to store the database connection
+db_connection = None
+
+def get_db_connection():
+    global db_connection
+    if db_connection is None:
+        creds = open('credentials.json', 'r')
+        credentials = json.load(creds)
+
+        # Attempt to establish a new connection if it doesn't exist
+        try:
+            db_connection = connection(credentials['host'], credentials['user'], credentials['password,'])
+        except Exception as e:
+            print("Can't Connect Without Credentials!", e)
+    return db_connection
+
+def to_json(header, data):
     json_data = {}
 
     # Add header tuple to JSON data
@@ -13,17 +30,15 @@ def toJson(header, data):
     for tup in data:
         value = tup[0]
         json_data[header_key].append(value)
-        
-    print("returned : ",json_data)
+
+    print("returned : ", json_data)
     return json_data
-
-
-app = Flask(__name__)
-
 
 
 @app.route('/data', methods=['GET', 'POST'])
 def handle_data():
+    global db_connection
+
     if request.method == 'POST':
         req_data = request.get_json()
         print("Received data:", req_data)
@@ -31,42 +46,44 @@ def handle_data():
         creds = 'credentials.json'
         query = "queries.json"
         
-        if("host" in req_data.keys()):
+        if "host" in req_data.keys():
             with open(creds, 'w') as json_file:
                 json.dump(req_data, json_file, indent=4)
-        if("query" in req_data.keys()):
+        if "query" in req_data.keys():
             with open(query, 'w') as json_file:
-                json.dump(req_data,json_file,indent=4)
+                json.dump(req_data, json_file, indent=4)
         
         
         return jsonify({"message": "Data received successfully"})
     
     elif request.method == 'GET':
         print("IN GET")
+        # Get the database connection
+        con = get_db_connection()
+        if con is None:
+            return jsonify({"error": "Unable to establish database connection"})
+
         creds = open('credentials.json','r')
         credentials = json.load(creds)
         
         queryFile = open('queries.json','r')
         queries = json.load(queryFile)
         
-        
-        con = None
-        
-        try:
-            con = connection(credentials['host'],credentials['user'],credentials['passwprd'])
-        except KeyError:
-            print("Can't Connect Without Credentials!")
-        
         cursor = con.cursor()
         
-        if(queries["query"]):
+        if "query" in queries:
             try:
                 cursor.execute(queries["query"])
                 resultSet = cursor.fetchall()
-            except:
-                pass
-        header = [desc[0] for desc in cursor.description]    
-        resultSet = toJson(header,resultSet)
+            except Exception as e:
+                print("Error executing query:", e)
+                resultSet = []
+        try:    
+            header = [desc[0] for desc in cursor.description]    
+            resultSet = to_json(header,resultSet)
+        except Exception as e:
+            print("Error converting to JSON:", e)
+            resultSet = {"status": "executed"}
         
         return jsonify(resultSet)
 
